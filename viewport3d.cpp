d9194efe7070a16e5b8468f825a9fa7abc563326
Abdul-AZ <abdulalzeidi@gmail.com>
2023-11-26 14:26:18 +0000

    Initial commit

---
+++ viewport3d.cpp
@@ -0,0 +1,6 @@
+#include "viewport3d.h"
+
+Viewport3D::Viewport3D(QWidget* parent) : QOpenGLWidget(parent)
+{
+
+}
---
+++ viewport3d.h
@@ -0,0 +1,12 @@
+#ifndef VIEWPORT3D_H
+#define VIEWPORT3D_H
+
+#include <QOpenGLWidget>
+
+class Viewport3D : public QOpenGLWidget
+{
+public:
+    Viewport3D(QWidget* parent = nullptr);
+};
+
+#endif // VIEWPORT3D_H
