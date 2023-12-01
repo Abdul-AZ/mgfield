b117261f424ea483a01a9ab0a98664a3929060e9
Abdul-AZ <abdulalzeidi@gmail.com>
2023-12-01 18:42:58 +0000

    Enable debug output to stdout in debug builds

--- main.cpp
+++ main.cpp
@@ -2,8 +2,47 @@
 
 #include <QApplication>
 
+
+#ifdef QT_DEBUG
+
+void
+DebugMessageOutput (QtMsgType type, const QMessageLogContext &context,
+                   const QString &msg)
+{
+    QByteArray localMsg = msg.toLocal8Bit ();
+    switch (type)
+    {
+    case QtDebugMsg:
+        fprintf (stderr, "Debug: %s (%s:%u, %s)\n", localMsg.constData (),
+                context.file, context.line, context.function);
+        break;
+    case QtInfoMsg:
+        fprintf (stderr, "Info: %s (%s:%u, %s)\n", localMsg.constData (),
+                context.file, context.line, context.function);
+        break;
+    case QtWarningMsg:
+        fprintf (stderr, "Warning: %s (%s:%u, %s)\n", localMsg.constData (),
+                context.file, context.line, context.function);
+        break;
+    case QtCriticalMsg:
+        fprintf (stderr, "Critical: %s (%s:%u, %s)\n", localMsg.constData (),
+                context.file, context.line, context.function);
+        break;
+    case QtFatalMsg:
+        fprintf (stderr, "Fatal: %s (%s:%u, %s)\n", localMsg.constData (),
+                context.file, context.line, context.function);
+        abort ();
+    }
+}
+
+#endif
+
 int main(int argc, char *argv[])
 {
+#ifdef QT_DEBUG
+    qInstallMessageHandler (DebugMessageOutput);
+#endif
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
