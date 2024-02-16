0dfb64fe7531a4920ef3adf0747ddad8ec6d512d
Abdulrahman Al Zeidi <abdulalzeidi@gmail.com>
2024-02-16 17:51:16 +0000

    Add calculating magnetic field for non-infinite length cables

--- src/sim/mfsimulator.cpp
+++ src/sim/mfsimulator.cpp
@@ -76,24 +76,68 @@ const QVector3D MFSimulator::GetPosition(int32_t x, int32_t y, int32_t z)
     return SimulationDatapointsSpacing * QVector3D(x - SimulationNumDatapointsX / 2, y - SimulationNumDatapointsY / 2, z - SimulationNumDatapointsZ / 2);
 }
 
-void MFSimulator::CalculateContributionsFromCable(const TransmissionCable& cable)
+static QVector3D CalculateContributionFromInfiniteCable(const QVector3D& point, const TransmissionCable& cable)
 {
     // Assuming cable is infinite in length
     // Using Biot-Savart Law to calculate magnitude of magnetic field
     // |B| = (u0 . I) / (2PI . h)
     // Where h is the shortest distance of point from cable
     const double pi = SIM_CONSTANT_PI;
+    QVector3D cableDirection = (cable.Rotation * QVector3D{1.0f, 0.0f, 0.0f}).normalized();
+
+    float h = point.distanceToLine(cable.Position, cableDirection);
+    float magnitude = cable.GetDCCurrent() / (2 * pi * h);
+    QVector3D direction =  QVector3D::crossProduct(cableDirection, cable.Position - point).normalized();
+
+    return magnitude * direction;
+}
+
+static QVector3D CalculateContributionFromfiniteCable(const QVector3D& point, const TransmissionCable& cable)
+{
+    const double pi = SIM_CONSTANT_PI;
+    QVector3D result;
+
+    QVector3D start = cable.Rotation.rotatedVector(QVector3D(1.0f, 0.0f, 0.0f));
+    QVector3D end = cable.Rotation.rotatedVector(QVector3D(-1.0f, 0.0f, 0.0f));
+
+    start += cable.Position;
+    end += cable.Position;
+
+    const int32_t n = 100;
+
+    const QVector3D deltaL = (end - start) / (float)n;
+
+    // Calculate Biot-Savart Law integral
+    for (int32_t i = 0; i < n; i++)
+    {
+        float t = (float)i / n;
+        QVector3D P = start + (end - start) * t;
+        QVector3D r = P - point;
+
+        if(r.lengthSquared() == 0)
+            continue;
+
+        QVector3D funcEval = (QVector3D::crossProduct(deltaL, r.normalized()))  / r.lengthSquared();
+
+        result += funcEval;
+    }
+
+    return result * cable.GetDCCurrent() / (4 * pi);
+}
+
+void MFSimulator::CalculateContributionsFromCable(const TransmissionCable& cable)
+{
     for (int x = 0; x < SimulationNumDatapointsX; x++)
         for (int y = 0; y < SimulationNumDatapointsY; y++)
             for (int z = 0; z < SimulationNumDatapointsZ; z++)
             {
                 QVector3D position = GetPosition(x, y, z);
-                QVector3D cableDirection = (cable.Rotation * QVector3D{1.0f, 0.0f, 0.0f}).normalized();
+                QVector3D result;
+                if(cable.GetIsInfiniteLength())
+                    result = CalculateContributionFromInfiniteCable(position, cable);
+                else
+                    result = CalculateContributionFromfiniteCable(position, cable);
 
-                float h = position.distanceToLine(cable.Position, cableDirection);
-                float magnitude = cable.GetDCCurrent() / (2 * pi * h);
-                QVector3D direction =  QVector3D::crossProduct(cableDirection, cable.Position - position).normalized();
-
-                SimulationResults[GetResultsElementIndex(x, y, z)] += magnitude * direction;
+                SimulationResults[GetResultsElementIndex(x, y, z)] = result;
             }
 }
